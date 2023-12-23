5e84f00a0d093bfe1135a8cb1c16bf3c0424215c
Jake Scott <coolasjake8@gmail.com>
2023-12-24 09:28:07 +1100

    laptop missile work

---
+++ Assets/Scripts/Weapons/MachineGun/Attacks/Missile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    [HideInInspector]
+    public MissileLauncher missileLauncher;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyAfterTime(5f));
+    }
+
+    private IEnumerator DestroyAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy)
+        {
+            missileLauncher.RodHit(this, enemy);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/MachineGun/MissileLauncher.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLauncher : Weapon
+{
+    public override WeaponType Type()
+    {
+        return WeaponType.MissileLauncher;
+    }
+
+    [Header("MissileLauncher Stats")]
+    public ModifiableFloat missileSpeed = new ModifiableFloat(10f, 0.01f, 1000f);
+    public ModifiableFloat explosionSize = new ModifiableFloat(1f, 0f, 100f);
+
+    [Header("MissileLauncher Refs")]
+
+    private Missile missilePrefab;
+
+    protected override bool Fire()
+    {
+        if (Time.time > _lastShot + FireRate)
+        {
+            Missile newMissile = Instantiate(missilePrefab, firingPoint.position, firingPoint.rotation);
+            Vector2 dir = firingPoint.up;
+            newMissile.GetComponent<Rigidbody2D>().velocity = dir * missileSpeed.Value;
+            newMissile.missileLauncher = this;
+            _lastShot = Time.time;
+        }
+        return true;
+    }
+
+    protected override void WeaponUpdate()
+    {
+
+    }
+
+    protected override void WeaponFixedUpdate()
+    {
+        
+    }
+
+    public void MissileHit(Missile missile, Enemy enemy)
+    {
+        if (enemy.CheckDodge(missile.transform.position))
+            return;
+        StaticRefs.SpawnExplosion(0.5f, missile.transform.position);
+        DefaultHit(enemy);
+    }
+
+    private void Explosion(Vector2 origin)
+    {
+        StaticRefs.SpawnExplosion(explosionSize.Value, origin);
+
+    }
+
+    public override void AddModifier(string statName, string modifierName, StatChangeOperation operation, float value)
+    {
+        switch (statName.ToLower())
+        {
+            case "bullet speed":
+                missileSpeed.AddModifier(modifierName, value, operation);
+                return;
+        }
+
+        base.AddModifier(statName, modifierName, operation, value);
+    }
+
+    public override void UnlockPower(string powerName, int level)
+    {
+        MissileLauncherPowers power;
+        if (Enum.TryParse(powerName, out power))
+        {
+            powers[(int)power] = level;
+            Debug.Log("Upgrading power: " + powerName + " to " + level);
+        }
+        else
+            Debug.Log("Couldn't find power with name: " + powerName);
+    }
+
+    protected override void Setup()
+    {
+        missilePrefab = attackPrefab.GetComponent<Missile>();
+        powers = new int[Enum.GetNames(typeof(MissileLauncherPowers)).Length];
+        SetupDamageTypes();
+    }
+
+    private enum MissileLauncherPowers
+    {
+        
+    }
+}
